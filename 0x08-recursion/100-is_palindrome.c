af0724cb82e69833b586a6b9486c429d5bf4f6f6
Fainus Gabriel <nusgabriel17@gmail.com>
2022-09-28 12:40:37 -0700

    correction of 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,7 +16,7 @@ int is_palindrome(char *s)
 
 /**
  * check - checks if a string is a palindrome
- * @S: string to be checked
+ * @s: string to be checked
  * @start: start index
  * @end: end index
  * @flag: flag to indicate if a string is a palindrome
