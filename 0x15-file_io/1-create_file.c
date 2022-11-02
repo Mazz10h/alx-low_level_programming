d87a6ed463a27f22108463da1ab193d986bbf961
Fainus Gabriel <nusgabriel17@gmail.com>
2022-11-02 07:58:04 -0700

    CRT task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,19 +17,19 @@ int _strlen(char *str)
 }
 
 /**
- * append_text_to_file - appends text at the end of a file
+ * create_file - creates a file
  * @filename: name of the file
- * @text_content: content to be appended
+ * @text_content: content of the file to be created
  *
  * Return: 1 on success, -1 otherwise
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int file, wrote;
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_WRONLY | O_APPEND);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 	if (text_content != NULL)
@@ -42,9 +42,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		}
 		close(file);
 		return (1);
-	{
-	else
 	}
+	else
+	{
 		close(file);
 		return (1);
 	}
